b7aaf82e571d93ba3627e97bfdb6d79fda683a29
Eric Clay <eric@candacecrowe.com>
2020-11-25 01:27:00 -0500

    1.4.1.1

--- assets/BRAGbook.js
+++ assets/BRAGbook.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-//BRAGbook™ 1.4.1.0
+//BRAGbook™ 1.4.1.1
 //© copyright 2013 Candace Crowe Design
 
 function reloadPage() {
--- bragbook-gallery.php
+++ bragbook-gallery.php
@@ -3,7 +3,7 @@
 Plugin Name: BRAGbook Gallery
 Plugin URI: http://www.bragbook.gallery/wp-plugin/
 Description: Installs necessary components to allow for easy implementation of the Bragbook before and after gallery from Candace Crowe Design.
-Version: 1.4.1.0
+Version: 1.4.1.1
 Author: Candace Crowe Design
 Author URI: http://www.candacecrowe.com/
 License: A "Slug" license name e.g. GPL2
